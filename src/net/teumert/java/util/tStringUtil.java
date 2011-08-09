9be7b59f6c5d5073dba7efd06c50567c71a895a7
Tr3kk3r <sebastian.teumert@gmx.de>
2011-08-09 17:36:05 +0200

    added method to justify mono spaced text [has quite nice distrubution of whitespaces]

--- src/net/teumert/java/util/tStringUtil.java
+++ src/net/teumert/java/util/tStringUtil.java
@@ -1,6 +1,10 @@
 package net.teumert.java.util;
 
+import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.Random;
+
+import de.lessvoid.nifty.tools.StringHelper;
 
 public class tStringUtil {
 	
@@ -41,5 +45,76 @@ public class tStringUtil {
 	    }
 	    return randomStrings;
 	}
+	
+	
+	public static void main(String[] args) {
+		String lipsum ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum hendrerit mauris, sit amet egestas quam rhoncus eu. Aenean quis est dui, ac tempus lacus. Aliquam sollicitudin dapibus ipsum, id egestas nulla commodo ac. Praesent pretium libero fringilla ante facilisis at rutrum ante interdum. Vestibulum vitae fringilla urna. Phasellus interdum mauris sed purus bibendum quis dignissim risus rhoncus. Aliquam risus dui, luctus id dignissim ac, tincidunt at mauris. Donec tincidunt, massa sed tempus ultricies, urna diam facilisis leo, volutpat venenatis est mauris vel metus. Nulla venenatis ultricies sagittis. Mauris pharetra nunc vitae mi placerat sit amet pellentesque tortor tristique. Sed pellentesque sagittis dolor non consectetur. Pellentesque id nunc metus, sed scelerisque risus. Nam fringilla ornare justo vitae placerat. Donec sit amet nisl ac augue pellentesque pretium at in metus. Donec faucibus, est eu dapibus porta, ligula ante fermentum diam, in mollis justo urna nec ipsum. Etiam nulla tellus, vestibulum et rutrum quis, faucibus a nisi. Ut consequat venenatis quam, ac porttitor quam vulputate sit amet. Nam id nisi eget odio sagittis condimentum in ac nisi. Phasellus venenatis tempor justo, id semper mauris facilisis vestibulum. Donec facilisis tellus sed orci viverra vehicula.";
+		justifyParagraph(lipsum, 40);
+	}
+	
+	public static String[] justifyParagraph(String message, int letters) {
+		
+		//remove line breaks
+		message = message.replace('\n', ' ');
+		
+		//remove double whitespace
+		String replaced = message.replace("  ", " ");
+		while(!message.equals(replaced)){
+			message = replaced;
+			replaced = message.replace("  ", " ");
+		}
+				
+		ArrayList<ArrayList<String>> lineWords = new ArrayList<ArrayList<String>>();
+		ArrayList<String> currentLine = new ArrayList<String>();
+		
+		String[] words = message.split(" ");
+		
+		// wrap lines
+		String lineBuffer = words[0];
+		currentLine.add(words[0]);
+		//start with second word
+		for (int i = 1; i < words.length; i++) {
+			String word = words[i];	
+			if((lineBuffer + word).length() < letters) {
+				lineBuffer += (" " + word);
+				currentLine.add(word);
+			}
+			else {
+				lineBuffer = justifyLine(currentLine, letters, (letters - lineBuffer.length()));
+				lineBuffer = word;
+				lineWords.add(currentLine);
+				currentLine = new ArrayList<String>();
+				currentLine.add(word);
+			}
+			
+		}
+		lineWords.add(currentLine);
+			
+		String[] lines = new String[lineWords.size()];		
+		return lineWords.toArray(lines);
+	}
+
+	private static String justifyLine(ArrayList<String> currentLine, int letters, int plusWhite) {
+		
+		String lineBuffer = "";		
+		
+		int holes = currentLine.size() - 1;
+		int add = plusWhite;
+		
+		for (int i = 1; i < currentLine.size(); i++) {
+			lineBuffer += " ";
+			while(add - holes >= 0) {
+				lineBuffer += " ";
+				add -= holes;			
+			}
+			lineBuffer += currentLine.get(i);
+			add += plusWhite;
+		}
+		
+		return lineBuffer;
+	}
+	
+	
+	
 	
 }
