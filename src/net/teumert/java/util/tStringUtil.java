3c5788b5a4b2bbf4b633a32c794d55f5795e8262
Tr3kk3r <sebastian.teumert@gmx.de>
2011-08-20 15:55:46 +0200

    fixed error, updated documentation

--- src/net/teumert/java/util/tStringUtil.java
+++ src/net/teumert/java/util/tStringUtil.java
@@ -3,10 +3,11 @@ package net.teumert.java.util;
 import java.util.ArrayList;
 import java.util.Random;
 
+// TODO add LGPL license hint here
+
 public class tStringUtil {
 	
 	/**
-	 * Currently unused	 
 	 * @param source Array containing the objects that should be imploded in string representation
 	 * @param delimiter Separator for the strings
 	 * @return String containing all objects in string representation separated by delimiter
@@ -14,54 +15,56 @@ public class tStringUtil {
 	public static String implode(Object[] source, String delimiter) {
 		StringBuilder sb = new StringBuilder(source[0].toString());
 		for (int i = 1; i < source.length; i++) {
-			if(source[i] == null)
-				break;
+			if (source[i] == null) break;
 			sb.append(delimiter);
 			sb.append(source[i].toString());
-		}		
+		}
 		return sb.toString();
 	}
 	
 	/**
-	 * {@link}http://stackoverflow.com/questions/4951997/generating-random-words-in-java
+	 * Generates random words, idea found on stackoverflow:
+	 * <a href="http://stackoverflow.com/questions/4951997/generating-random-words-in-java">
+	 * http://stackoverflow.com/questions/4951997/generating-random-words-in-java</a>
+	 * 
 	 * @param numberOfWords
 	 * @return
+	 * 
 	 */
-	public static String[] generateRandomWords(int numberOfWords)
-	{
-	    String[] randomStrings = new String[numberOfWords];
-	    Random random = new Random();
-	    for(int i = 0; i < numberOfWords; i++)
-	    {
-	        char[] word = new char[random.nextInt(8)+3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
-	        for(int j = 0; j < word.length; j++)
-	        {
-	            word[j] = (char)('a' + random.nextInt(26));
-	        }
-	        randomStrings[i] = new String(word);
-	    }
-	    return randomStrings;
+	public static String[] generateRandomWords(int numberOfWords) {
+		String[] randomStrings = new String[numberOfWords];
+		Random random = new Random();
+		for (int i = 0; i < numberOfWords; i++) {
+			char[] word = new char[random.nextInt(8) + 3]; // words of length 3 through 10. (1 and 2 letter
+									// words are boring.)
+			for (int j = 0; j < word.length; j++) {
+				word[j] = (char) ('a' + random.nextInt(26));
+			}
+			randomStrings[i] = new String(word);
+		}
+		return randomStrings;
 	}
 	
-	
-	public static void main(String[] args) {
-		String lipsum ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum hendrerit mauris, sit amet egestas quam rhoncus eu. Aenean quis est dui, ac tempus lacus. Aliquam sollicitudin dapibus ipsum, id egestas nulla commodo ac. Praesent pretium libero fringilla ante facilisis at rutrum ante interdum. Vestibulum vitae fringilla urna. Phasellus interdum mauris sed purus bibendum quis dignissim risus rhoncus. Aliquam risus dui, luctus id dignissim ac, tincidunt at mauris. Donec tincidunt, massa sed tempus ultricies, urna diam facilisis leo, volutpat venenatis est mauris vel metus. Nulla venenatis ultricies sagittis. Mauris pharetra nunc vitae mi placerat sit amet pellentesque tortor tristique. Sed pellentesque sagittis dolor non consectetur. Pellentesque id nunc metus, sed scelerisque risus. Nam fringilla ornare justo vitae placerat. Donec sit amet nisl ac augue pellentesque pretium at in metus. Donec faucibus, est eu dapibus porta, ligula ante fermentum diam, in mollis justo urna nec ipsum. Etiam nulla tellus, vestibulum et rutrum quis, faucibus a nisi. Ut consequat venenatis quam, ac porttitor quam vulputate sit amet. Nam id nisi eget odio sagittis condimentum in ac nisi. Phasellus venenatis tempor justo, id semper mauris facilisis vestibulum. Donec facilisis tellus sed orci viverra vehicula.";
-		justifyParagraph(lipsum, 40);
-	}
-	
+	/**
+	 * Justifies a given paragraph of text. Only works with mono-spaced fonts.
+	 * 
+	 * @param message the text to be justified
+	 * @param letters the amount of letters that should be placed on one line.
+	 * @return array of strings, each element of the array is one justified line.
+	 */
 	public static String[] justifyParagraph(String message, int letters) {
 		
-		//remove line breaks
+		// remove line breaks
 		message = message.replace('\n', ' ');
 		
-		//remove double whitespace
+		// remove double whitespace
 		String replaced = message.replace("  ", " ");
-		while(!message.equals(replaced)){
+		while (!message.equals(replaced)) {
 			message = replaced;
 			replaced = message.replace("  ", " ");
 		}
-				
-		ArrayList<ArrayList<String>> lineWords = new ArrayList<ArrayList<String>>();
+		
+		ArrayList<String> lineWords = new ArrayList<String>();
 		ArrayList<String> currentLine = new ArrayList<String>();
 		
 		String[] words = message.split(" ");
@@ -69,49 +72,54 @@ public class tStringUtil {
 		// wrap lines
 		String lineBuffer = words[0];
 		currentLine.add(words[0]);
-		//start with second word
+		// start with second word
 		for (int i = 1; i < words.length; i++) {
-			String word = words[i];	
-			if((lineBuffer + word).length() < letters) {
+			String word = words[i];
+			if ((lineBuffer + word).length() < letters) {
 				lineBuffer += (" " + word);
 				currentLine.add(word);
 			}
 			else {
-				lineBuffer = justifyLine(currentLine, letters, (letters - lineBuffer.length()));
+				lineBuffer = justifyLine(currentLine, (letters - lineBuffer.length()));
+				lineWords.add(lineBuffer);
 				lineBuffer = word;
-				lineWords.add(currentLine);
 				currentLine = new ArrayList<String>();
 				currentLine.add(word);
 			}
 			
 		}
-		lineWords.add(currentLine);
-			
-		String[] lines = new String[lineWords.size()];		
+		
+		String[] lines = new String[lineWords.size()];
 		return lineWords.toArray(lines);
 	}
-
-	private static String justifyLine(ArrayList<String> currentLine, int letters, int plusWhite) {
+	
+	/**
+	 * Method internally used by justifyParagraph() in order to justify one line
+	 * 
+	 * @param currentLine words that shall be placed on that line
+	 * @param plusWhite number of additional whitespace needed.
+	 * @return String containing the justified line.
+	 */
+	private static String justifyLine(ArrayList<String> currentLine, int plusWhite) {
 		
-		String lineBuffer = "";		
+		String lineBuffer = "";				// contains the line
 		
-		int holes = currentLine.size() - 1;
-		int add = plusWhite;
+		int holes = currentLine.size() - 1; 		// there is one empty space between the words less then there are words
+		int add = plusWhite;				// we need to add all whitespace
 		
-		for (int i = 1; i < currentLine.size(); i++) {
-			lineBuffer += " ";
-			while(add - holes >= 0) {
-				lineBuffer += " ";
-				add -= holes;			
+		lineBuffer = currentLine.get(0);		// initialise buffer with first word (no leading whitespace!)
+		for (int i = 1; i < currentLine.size(); i++) {  // iterate over all words, starting with second word
+								// (offset 1)
+			lineBuffer += " ";			// add at least one whitespace before inserting next word.
+			while (add - holes >= 0) {		// now comes a mathematical trick avoiding the use of rational numbers
+				lineBuffer += " ";		// add whitespace
+				add -= holes;			// do the trick again ;) 
 			}
-			lineBuffer += currentLine.get(i);
-			add += plusWhite;
+			lineBuffer += currentLine.get(i);	// add word
+			add += plusWhite;			// and the last step of the math trick
 		}
-		
 		return lineBuffer;
-	}
-	
-	
-	
-	
+		
+		//if you are interested in the math behind it, checkout readme-string-util.txt in this folder
+	}	
 }
