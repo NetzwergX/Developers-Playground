1e7cf13aaac7ddf0b146e9d8d2e63d04272a5425
NetzwergX <sebastian.teumert@gmx.de>
2012-01-11 13:47:42 +0100

    Added repository update script

---
+++ update-repos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# GIT Repository update script by Sebastian Teumert
+# !Important! 
+# Pulls & pushes only the 'master' branch
+# Remotes have to be named 'origin' for pulling and 'upstream' for pushing
+# If no remote named 'upstream' exists, the repo will not be pushed
+
+
+FILES="$PWD/*/"
+for f in $FILES
+do
+  echo "########################################"
+  echo "Processing $f ..." 
+  cd $f    
+  test -d "$f.git/" && echo "GIT repository exists" || echo "GIT repository does not exists"
+  if [ -d "$f.git/" ];
+  then
+   git pull origin master   
+   git remote show | grep upstream > /dev/null && git push upstream master
+  fi
+  cd ..
+done
+
